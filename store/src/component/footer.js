de10b5dbd0dfe89a6fa1bd34d761938cc15be99a
ZaidKairallah <zaidkhairallahjarrah@gmail.com>
2026-01-21 11:55:46 +0300

    jjj

---
+++ store/src/component/footer.js
@@ -0,0 +1,10 @@
+//
+import '../css1/footer.css'
+ export default function Footer(){
+    return(
+        <div id="footer">
+            <p> this is footer</p>
+
+        </div>
+    )
+}
---
+++ store/src/component/navbar.js
@@ -0,0 +1,22 @@
+//
+import'../css1/nav.css'
+function Navbar(){
+    return(
+
+        <div id="nav">
+            <ul>
+
+<li>Home</li>
+<li>prudoct</li>
+<li>About</li>
+<li>content</li>
+
+
+
+            </ul>
+
+        </div>
+
+    )
+}
+export default Navbar;
